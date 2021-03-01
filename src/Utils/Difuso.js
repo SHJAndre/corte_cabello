b0e55393c9e3bab146522f846a83b544dde0e04d
Claudiars20 <Claudiars20@users.noreply.github.com>
2021-03-01 12:39:12 -0500

    ModuloInferencial

--- src/Utils/Difuso.js
+++ src/Utils/Difuso.js
@@ -1,3 +1,5 @@
+import Busqueda from "./InferenciaCorte"
+
 function triangular(x,param)
 {
   var a=parseFloat(param[0]);
@@ -219,7 +221,7 @@ function ColorCabello(Escala)
     }
 }
 
-function ColorCabello(Escala)
+function ColorPiel(Escala)
 {
     //-------------------------------------------------------------------------------
     //Universo para variable Color de cabello
@@ -262,10 +264,8 @@ function Personalidad(Escala)
     var gReservado  = triangular(uPersonalidad, cReservado)
     var gAlocado = triangular(uPersonalidad,cAlocado)
 
-    //Grado de membresía para Escala
-    var Personalidad = 5
-    var gPersonalidadReservado  = triangular(Personalidad,cReservado)
-    var gPersonalidadAlocado = triangular(Personalidad,cAlocado)
+    var gPersonalidadReservado  = triangular(Escala,cReservado)
+    var gPersonalidadAlocado = triangular(Escala,cAlocado)
 
     var Reservado = gPersonalidadReservado*100;
     var Alocado = gPersonalidadAlocado*100;
@@ -279,193 +279,87 @@ function Personalidad(Escala)
     }
 }
 
-function TipoCabello(Escala)
+function TipoCabe(Escala)
 {
-    
+    //-------------------------------------------------------------------------------
+    // Universo para variable Tipo de Cabello
+    var uTipoCabello = linspace(0,10, 100)
+
+    //-------------------------------------------------------------------------------
+    //Parametros de conjuntos difusos: var Tipo de Cabello
+    var cLacio = [0,4,7.5]     
+    var cCrespo = [3,7,10]          
+    //-------------------------------------------------------------------------------
+    //Grado de membresía para universo de Tipo de Cabello
+    var gLacio  = triangular(uTipoCabello, cLacio)
+    var gCrespo = triangular(uTipoCabello, cCrespo)
+
+    //Grado de membresía para Tipo de Cabello
+    var gCabelloLacio = triangular(Escala, cLacio)
+    var gCabelloCrespo = triangular(Escala, cCrespo)
+
+    //-------------------------------------------------------------------------------
+    var Lacio = gCabelloLacio*100;
+    var Crespo = gCabelloCrespo*100;
+    if (Lacio>Crespo)
+    {
+        return "gLacio";
+    }
+    else 
+    {
+        return "gCrespo";
+    }
 }
 
-
-export default function Tamanio(Edad,Temperatura,IMC)
+function Ocupacion(Escala)
 {
-  //-------------------------EDAD-------------------------------------------------------------
-  //Universo para variable Edad (25-70 años)
-  var uEdad=linspace(15,75,200);
-  //-------------------------------------------------------------------------------
-  //Parametros de conjuntos difusos: var Edad
-  var cJoven = [15,25,60];     
-  var cAdulto = [35,52.5,70];        
-  var cAnciano = [60,70,75];      
-  //-------------------------------------------------------------------------------
-  // Grado de membresía para universo de Edad
-  var gJoven  = triangular(uEdad, cJoven);
-  var gAdulto = triangular(uEdad, cAdulto);
-  var gAnciano = triangular(uEdad, cAnciano);
+    //-------------------------------------------------------------------------------
+    //Universo para variable Nivel de Ocupacion
+    var uNivelOcupacion = linspace(0,10, 100)
 
-  //Grado de membresía para Edad = 50 años
-  //var Edad = 45
-  var gJovenEdad = triangular(Edad, cJoven);
-  var gAdultoEdad = triangular(Edad, cAdulto);
-  var gAncianoEdad = triangular(Edad, cAnciano);
-  //console.log(gJovenEdad);
-  //console.log(gAdultoEdad);
-  //console.log(gAncianoEdad);
-  //-------------------------AMBIENTE-------------------------------------------------------------
-  //Universo para variable Edad (25-70 años)
-  var uTemperatura = linspace(0,35, 200);
-  //-------------------------------------------------------------------------------
-  //Parametros de conjuntos difusos: var Edad
-  var cFrio = [0,5,13];   
-  var cTemplado = [10,16,23];        
-  var cCalido = [20,28,35];      
+    //-------------------------------------------------------------------------------
+    //Parametros de conjuntos difusos: var Nivel de Ocupacion
+    var cLibre = [0,3,6.5]     
+    var cMucho = [2,7,10]          
+    //-------------------------------------------------------------------------------
+    //Grado de membresía para universo de Nivel de Ocupacion
+    var gLibre  = triangular(uNivelOcupacion, cLibre)
+    var gMucho = triangular(uNivelOcupacion, cCrespo)
 
-  //-------------------------------------------------------------------------------
-  // Grado de membresía para universo de Edad
-  var gFrio  = triangular(uTemperatura, cFrio);
-  var gTemplado = triangular(uTemperatura, cTemplado);
-  var gCalido = triangular(uTemperatura, cCalido);
+    //Grado de membresía para Nivel de Ocupacion
+    var gOLibre = triangular(Escala, cLibre)
+    var gOMucho = triangular(Escala, cCrespo)
 
-  //Grado de membresía para Edad = 50 años
-  //var Temperatura = 20;
-  var gClimaFrio  = triangular(Temperatura, cFrio);
-  var gClimaTemplado = triangular(Temperatura, cTemplado);
-  var gClimaCalido = triangular(Temperatura, cCalido);
-  //console.log(gClimaFrio);
-  //console.log(gClimaTemplado);
-  //console.log(gClimaCalido);
-  //-------------------------IMC-------------------------------------------------------------
-  //Universo para variable Cantidad de ropa (1-7kg)
-  var uContextura = linspace(14, 35, 200);
-
-  //-------------------------------------------------------------------------------
-  //Parametros de conjuntos difusos: var Cantidad de ropa
-  var cBajoPeso = [14,14,18,19];    
-  var cNormal = [18,19,24.5,25.5];     
-  var cSobrePeso = [24.5,25.5,29.5,30.5];        
-  var cObesidad = [29.5,30.5,35,35];
-
-  //-------------------------------------------------------------------------------
-  //Grado de membresía para universo de Cantidad de ropa
-  var gBajoPeso  = trapezoidal(uContextura, cBajoPeso);
-  var gNormal = trapezoidal(uContextura, cNormal);
-  var gSobrePeso = trapezoidal(uContextura, cSobrePeso);
-  var gObesidad = trapezoidal(uContextura, cObesidad);
-
-  //Grado de membresía para Cantidad de ropa = 5.2
-  //var IMC = 19.8
-  var gBajoPesoContex  = trapezoidal(IMC, cBajoPeso);
-  var gNormalContex  = trapezoidal(IMC, cNormal);
-  var gSobrePesoContex  = trapezoidal(IMC, cSobrePeso);
-  var gObesidadContex  = trapezoidal(IMC, cObesidad);
-  //console.log(gBajoPesoContex);
-  //console.log(gNormalContex);
-  //console.log(gSobrePesoContex);
-  //console.log(gObesidadContex);
-  //-------------------------OUTPUT-------------------------------------------------------------
-  //----------------------TAMAÑO DE CORTE--------------------------
-  //Universo para variable Prestamo(S/.)
-  var uTamCorte = linspace(7, 80, 200);
-
-  //-------------------------------------------------------------------------------
-  //Parametros de conjuntos difusos: var Prestamo(S/.)  
-  var cCorto = [7,21,35];       
-  var cMediano = [30,40,65];      
-  var cLargo = [60,70,80];      
-
-  //-------------------------------------------------------------------------------
-  //Grado de membresía para universo de Prestamo(S/.)
-  var gCorto = triangular(uTamCorte, cCorto);
-  var gMediano = triangular(uTamCorte, cMediano);
-  var gLargo = triangular(uTamCorte, cLargo);
-  //-----------------------------------------INFERENCIA LOGICA-------------------------
-  var R1= Math.min(gJovenEdad,gClimaFrio,gBajoPesoContex);
-  var R2= Math.min(gJovenEdad,gClimaFrio,gNormalContex);
-  var R3= Math.min(gJovenEdad,gClimaFrio,gSobrePesoContex);	
-  var R4= Math.min(gJovenEdad,gClimaFrio,gObesidadContex);	
-  var R5= Math.min(gJovenEdad,gClimaTemplado,gBajoPesoContex);	
-  var R6= Math.min(gJovenEdad,gClimaTemplado,gNormalContex);	
-  var R7= Math.min(gJovenEdad,gClimaTemplado,gSobrePesoContex);	
-  var R8= Math.min(gJovenEdad,gClimaTemplado,gObesidadContex);	
-  var R9= Math.min(gJovenEdad,gClimaCalido,gBajoPesoContex);	
-  var R10= Math.min(gJovenEdad,gClimaCalido,gNormalContex);	
-  var R11= Math.min(gJovenEdad,gClimaCalido,gSobrePesoContex);	
-  var R12= Math.min(gJovenEdad,gClimaCalido,gObesidadContex);	
-  var R13= Math.min(gAdultoEdad,gClimaFrio,gBajoPesoContex);	
-  var R14= Math.min(gAdultoEdad,gClimaFrio,gNormalContex);	
-  var R15= Math.min(gAdultoEdad,gClimaFrio,gSobrePesoContex);	
-  var R16= Math.min(gAdultoEdad,gClimaFrio,gObesidadContex);	
-  var R17= Math.min(gAdultoEdad,gClimaTemplado,gBajoPesoContex);	
-  var R18= Math.min(gAdultoEdad,gClimaTemplado,gNormalContex);	
-  var R19= Math.min(gAdultoEdad,gClimaTemplado,gSobrePesoContex);	
-  var R20= Math.min(gAdultoEdad,gClimaTemplado,gObesidadContex);	
-  var R21= Math.min(gAdultoEdad,gClimaCalido,gBajoPesoContex);	
-  var R22= Math.min(gAdultoEdad,gClimaCalido,gNormalContex);	
-  var R23= Math.min(gAdultoEdad,gClimaCalido,gSobrePesoContex);	
-  var R24= Math.min(gAdultoEdad,gClimaCalido,gObesidadContex);	
-  var R25= Math.min(gAncianoEdad,gClimaFrio,gBajoPesoContex);	
-  var R26= Math.min(gAncianoEdad,gClimaFrio,gNormalContex);	
-  var R27= Math.min(gAncianoEdad,gClimaFrio,gSobrePesoContex);
-  var R28= Math.min(gAncianoEdad,gClimaFrio,gObesidadContex);
-  var R29= Math.min(gAncianoEdad,gClimaTemplado,gBajoPesoContex);	
-  var R30= Math.min(gAncianoEdad,gClimaTemplado,gNormalContex);	
-  var R31= Math.min(gAncianoEdad,gClimaTemplado,gSobrePesoContex);	
-  var R32= Math.min(gAncianoEdad,gClimaTemplado,gObesidadContex);	
-  var R33= Math.min(gAncianoEdad,gClimaCalido,gBajoPesoContex);
-  var R34= Math.min(gAncianoEdad,gClimaCalido,gNormalContex);
-  var R35= Math.min(gAncianoEdad,gClimaCalido,gSobrePesoContex);	
-  var R36= Math.min(gAncianoEdad,gClimaCalido,gObesidadContex);	
-
-  var Input = [R1, R2, R3, R4, R5, R6, R7, R8, R9, R10,R11, R12, R13, R14, R15, R16, R17, R18,R19,R20,R21, R22, R23, R24, R25, R26, R27, R28, R29, R30,R31, R32, R33, R34, R35, R36];
-
-  var Output =[gLargo,gLargo,gLargo,gMediano,gLargo,gLargo,gMediano,gCorto,gMediano,gMediano,gCorto,gCorto,gLargo,gLargo,gMediano,gMediano,gLargo,gMediano,gMediano,gMediano,gMediano,gCorto,gCorto,gCorto,gMediano,gMediano,gMediano,gMediano,gMediano,gMediano,gMediano,gCorto,gMediano,gCorto,gCorto];
-
-  var cR1 = cortar(R1,gLargo );
-  var cR2 = cortar(R2,gLargo );
-  var cR3 = cortar(R3,gLargo );
-  var cR4 = cortar(R4,gMediano);
-  var cR5 = cortar(R5,gLargo );
-  var cR6 = cortar(R6,gLargo );
-  var cR7 = cortar(R7,gMediano);
-  var cR8 = cortar(R8,gCorto);
-  var cR9 = cortar(R9,gMediano);
-  var cR10 = cortar(R10,gMediano);
-  var cR11 = cortar(R11,gCorto);
-  var cR12 = cortar(R12,gCorto);
-  var cR13 = cortar(R13,gLargo );
-  var cR14 = cortar(R14,gLargo );
-  var cR15 = cortar(R15,gMediano);
-  var cR16 = cortar(R16,gMediano);
-  var cR17 = cortar(R17,gLargo );
-  var cR18 = cortar(R18,gMediano);
-  var cR19 = cortar(R19,gMediano);
-  var cR20 = cortar(R20,gMediano);
-  var cR21 = cortar(R21,gMediano)
-  var cR22 = cortar(R22,gCorto);
-  var cR23 = cortar(R23,gCorto);
-  var cR24 = cortar(R24,gCorto);
-  var cR25 = cortar(R25,gMediano);
-  var cR26 = cortar(R26,gMediano);
-  var cR27 = cortar(R27,gMediano);
-  var cR28 = cortar(R28,gMediano);
-  var cR29 = cortar(R29,gMediano);
-  var cR30 = cortar(R30,gMediano);
-  var cR31 = cortar(R31,gMediano);
-  var cR32 = cortar(R32,gCorto);
-  var cR33 = cortar(R33,gMediano);
-  var cR34 = cortar(R34,gCorto);
-  var cR35 = cortar(R35,gCorto);
-  var cR36 = cortar(R36,gCorto);
-
-
-  var CORTE = union([cR1, cR2, cR3, cR4, cR5, cR6, cR7, cR8, cR9, cR10,cR11, cR12, cR13, cR14, cR15, cR16, cR17, cR18, cR19, cR20,cR21, cR22, cR23, cR24, cR25, cR26, cR27, cR28, cR29, cR30,cR31, cR32, cR33, cR34,cR35,cR36]);
-  //--------------------------------------DEFUZZIFICACION------------------------
-  //Aplicamos defuzzificación mediante CENTROIDE
-  var Y1 = defuzzificacion(uTamCorte, CORTE);
-  console.log(Y1);
-  return Y1;
+    //-------------------------------------------------------------------------------
+    var Libre = gOLibre*100;
+    var Mucho = gOMucho*100;
+    if (Libre>Mucho)
+    {
+        return "gLibre";
+    }
+    else 
+    {
+        return "gMucho";
+    }
 }
 
-var Edad=45;
-var Temperatura=20;
-var IMC=19.8;
-Tamanio(Edad,Temperatura,IMC);  
+export default function CorteRecomendado(Opacidad,TonoPiel,FormaCara,Estilo,TipoPersonalidad,TipoCabello,NivelOcup)
+{
+    // Pertenencia
+    var ColorCab = ColorCabello(Opacidad);
+    var ColorP = ColorPiel(TonoPiel); 
+    var Person = Personalidad(TipoPersonalidad);
+    var TipoCab = TipoCabe(TipoCabello);
+    var NivelOcup = NivelOcupacion(NivelOcup);
+    //-------------------------------------------
+    var Resultado = Busqueda(ColorCab,ColorP,FormaCara,Estilo,Person,TipoCab,NivelOcup)
+    return Resultado;
+}
+
+var CC=6;
+var CP=4;
+var P=4;
+var TipoCabello = 3;
+var Nivel = 8;
+console.log(CorteRecomendado(CC,CP,"gOvalado","gClasico",P,TipoCabello,Nivel));  
 
--- src/Utils/InferenciaCorte.js
+++ src/Utils/InferenciaCorte.js
@@ -1,11 +1,12 @@
-import "./KnowledgeBase/Varon.json"
-import "./KnowledgeBase/Mujer.json"
 //Varon nombre del arreglo jason
 //input variables de entrada caracteristicas
 //output urlCorte de imagen de corte
-function Busqueda(Varon,pcolorCabello,pcolorPiel,pformaRostro,pestilo,patrevimiento,ptipoCabello,pocupamiento)
+
+const Varon = require('./Varon.json');
+const Mujer = require('./Mujer.json');
+export default function Busqueda(pcolorCabello,pcolorPiel,pformaRostro,pestilo,patrevimiento,ptipoCabello,pocupamiento)
 {
-  var PersonaEncontrada=_.find(Varon, function(persona){
+  var PersonaEncontrada=_.find(Mujer, function(persona){
     return ((persona.colorCabello==pcolorCabello)&&(persona.colorPiel==pcolorPiel)&&  (persona.formaRostro==pformaRostro)&&(persona.estilo==pestilo)&&(persona.atrevimiento==patrevimiento)&&(persona.tipoCabello==ptipoCabello)&&(persona.ocupamiento==pocupamiento));
     });
   var Link=PersonaEncontrada.urlCorte;
